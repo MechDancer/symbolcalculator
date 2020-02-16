a7e733cbe804b3c13e699f2c6f24535b8efe62c1
Ydr <ydrml@hotmail.com>
2020-02-16 15:00:58 +0800

    update，歇了

--- src/test/kotlin/org/mechdancer/symbol/main.kt
+++ src/test/kotlin/org/mechdancer/symbol/main.kt
@@ -15,7 +15,6 @@ fun main() {
         println(dy)
         println(dxy)
 
-        println((y * y * y).substitute(x, Constant(.0)))
         println(e.substitute { this[x] = 4; this[y] = 10 })
     }
     println()
@@ -23,6 +22,13 @@ fun main() {
         val x by variable
         val y by variable
         val e = sqrt((x pow 2) + (y pow 2))
+        println(e)
+        println(e d x)
+    }
+    println()
+    run {
+        val x by variable
+        val e = ln(9 * x - 7)
         println(e)
         println(e d x)
     }
