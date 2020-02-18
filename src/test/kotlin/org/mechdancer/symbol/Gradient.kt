d5adeecbca608dbe08a0083e9624fffad0f658ea
Ydr <ydrml@hotmail.com>
2020-02-18 17:18:04 +0800

    增加测量误差（恐怖）

--- src/test/kotlin/org/mechdancer/symbol/Gradient.kt
+++ src/test/kotlin/org/mechdancer/symbol/Gradient.kt
@@ -3,6 +3,7 @@ package org.mechdancer.symbol
 import org.mechdancer.algebra.function.vector.euclid
 import org.mechdancer.algebra.implement.vector.Vector3D
 import org.mechdancer.algebra.implement.vector.vector3D
+import java.util.*
 import kotlin.math.abs
 
 private val beacons =
@@ -14,28 +15,41 @@ private val beacons =
 private val mobile =
     vector3D(15, 20, -3)
 
+private val engine = Random()
+
+fun measure(b: Vector3D, m: Vector3D) =
+    (b euclid m) * (1 + 5E-3 * engine.nextGaussian()) + 1E-2 * engine.nextGaussian()
+
 fun main() {
     val space = variables("x", "y", "z")
-    val map = beacons.map { it.toField() to (it euclid mobile) }
+    val map = beacons.map { it.toField() to measure(it, mobile) }
+
+    var t0 = System.currentTimeMillis()
 
     val e = map.sumBy { (beacon, measure) ->
         ((space.ordinaryField - beacon).length - measure) `^` 2
     }
-    val grad = space.gradientOf(e)
+    val gradField = space.gradientOf(e)
+
+    println("求梯度耗时 = ${System.currentTimeMillis() - t0}ms")
 
     val actual = mobile.toField()
     var p = vector3D(1, 0, -1).toField()
     val pid = PIDLimiter(.4, .02, .02, 30.0)
     for (i in 1..200) {
-        val temp = grad.substitute(p)
-        val l = (temp.length as Constant).value
+        t0 = System.currentTimeMillis()
+
+        val grad = gradField.substitute(p)
+        val l = (grad.length as Constant).value
         val k = pid(l)
-        val step = temp * (k / l)
+        val step = grad * (k / l)
         p -= step
 
-        println(p)
+        val t = System.currentTimeMillis() - t0
 
+        println(p)
         println("迭代次数 = $i")
+        println("迭代耗时 = ${t}ms")
         println("步长 = $k")
         println("损失 = ${e.substitute(p)}")
         println("误差 = ${(p - actual).length}")
