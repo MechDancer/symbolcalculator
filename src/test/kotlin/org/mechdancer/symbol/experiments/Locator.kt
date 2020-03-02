bf89b4d0d8dd7e4818f4acfad37bce307e61a0cc
Ydr <ydrml@hotmail.com>
2020-03-02 21:51:08 +0800

    实现定位器类

---
+++ src/test/kotlin/org/mechdancer/symbol/experiments/Locator.kt
@@ -0,0 +1,74 @@
+package org.mechdancer.symbol.experiments
+
+import org.mechdancer.algebra.function.vector.plus
+import org.mechdancer.algebra.function.vector.times
+import org.mechdancer.algebra.implement.vector.Vector3D
+import org.mechdancer.algebra.implement.vector.vector3D
+import org.mechdancer.symbol.*
+import org.mechdancer.symbol.core.Constant
+import org.mechdancer.symbol.core.Expression
+import org.mechdancer.symbol.core.FunctionExpression
+import org.mechdancer.symbol.core.Variable
+import org.mechdancer.symbol.experiments.Locator.State.Preparing
+import org.mechdancer.symbol.experiments.Locator.State.Working
+import org.mechdancer.symbol.linear.ExpressionVector
+import kotlin.math.ln
+
+class Locator(beacons: List<Vector3D>) {
+    enum class State { Preparing, Working }
+
+    var state = Preparing
+        private set
+
+    private var last = vector3D(0, 0, -3)
+    private val struct = beacons.map { (space.ordinaryField - it.toPoint()).length() }
+
+    operator fun invoke(list: List<Double>): Vector3D {
+        val error = struct
+            .zip(list) { e, l -> if (l > 0) (e - l) `^` 2 else Constant.`-1` }
+            .filterIsInstance<FunctionExpression>()
+            .let { it.sum() / it.size }
+        val f = dampingNewton(error, space, .2)
+
+        val init = last.toPoint()
+        val (result, _) = recurrence(init to .0) { (p, _) -> f(p) }.firstOrLast { (_, s) -> s < 5e-6 }
+        val new = 3 - ln(error[result])
+        val old = 3 - ln(error[init])
+        val p = result.toVector()
+        when (state) {
+            Preparing -> {
+                last = when {
+                    new > 3   -> {
+                        state = Working
+                        (old / new).let { k -> last * k + p * (1 - k) }
+                    }
+                    new > old -> (old / new).let { k -> last * k + p * (1 - k) }.copy(z = -3.0)
+                    else      -> last
+                }
+            }
+            Working   ->
+                if (new > old) last = (old / new).let { k -> last * k + p * (1 - k) }
+        }
+        return p
+    }
+
+    private companion object {
+        private val vx = Variable("x")
+        private val vy = Variable("y")
+        private val vz = Variable("z")
+        private val space = variables("x", "y", "z")
+
+        private fun Vector3D.toPoint() =
+            ExpressionVector(mapOf(vx to Constant(x),
+                                   vy to Constant(y),
+                                   vz to Constant(z)))
+
+        private fun ExpressionVector.toVector() =
+            vector3D(this[vx]!!.toDouble(),
+                     this[vy]!!.toDouble(),
+                     this[vz]!!.toDouble())
+
+        private operator fun Expression.get(values: ExpressionVector) =
+            substitute(values).toDouble()
+    }
+}
--- src/test/kotlin/org/mechdancer/symbol/experiments/e3.kt
+++ src/test/kotlin/org/mechdancer/symbol/experiments/e3.kt
@@ -3,95 +3,68 @@ package org.mechdancer.symbol.experiments
 import org.mechdancer.algebra.core.Vector
 import org.mechdancer.algebra.core.rowView
 import org.mechdancer.algebra.doubleEquals
-import org.mechdancer.algebra.function.vector.div
-import org.mechdancer.algebra.function.vector.euclid
-import org.mechdancer.algebra.function.vector.minus
-import org.mechdancer.algebra.function.vector.plus
+import org.mechdancer.algebra.function.vector.*
 import org.mechdancer.algebra.implement.vector.Vector3D
 import org.mechdancer.algebra.implement.vector.toListVector
 import org.mechdancer.algebra.implement.vector.vector3D
 import org.mechdancer.remote.presets.remoteHub
-import org.mechdancer.symbol.*
-import org.mechdancer.symbol.core.Constant
-import org.mechdancer.symbol.core.Variable
-import org.mechdancer.symbol.linear.ExpressionVector
+import org.mechdancer.symbol.networksInfo
+import org.mechdancer.symbol.paint
+import org.mechdancer.symbol.paintFrame3
 import kotlin.math.abs
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
-private fun Vector3D.toPoint() =
-    ExpressionVector(mapOf(Variable("x") to Constant(x),
-                           Variable("y") to Constant(y),
-                           Variable("z") to Constant(z)))
-
 private const val maxMeasure = 30.0
-private val interval = maxMeasure / 2 / sqrt(2.0)
+private val interval = maxMeasure / 2 / sqrt(2.0) * .99
 
 // 地图
-private val BEACONS = listOf(
+private val beacons = listOf(
     vector3D(-interval, -interval, 0),
-    vector3D(-interval, 0, 0),
     vector3D(-interval, interval, 0),
-    vector3D(0, interval, 0),
     vector3D(interval, interval, 0),
-    vector3D(interval, 0, 0),
-    vector3D(interval, -interval, 0),
-    vector3D(0, -interval, 0))
+    vector3D(interval, -interval, 0))
 
-// 测量函数
+// 噪声
 
 private val engine = java.util.Random()
-private fun measure(d: Double) = d * (1 + 1e-3 * engine.nextGaussian()) + 5e-3 * engine.nextGaussian()
+private fun gaussian(sigma: Double) = sigma * engine.nextGaussian()
+
+private fun measure(d: Double) = d * (1 + gaussian(1e-3)) + gaussian(5e-3)
+private fun deploy(p: Vector3D) = p + vector3D(gaussian(.04), gaussian(.04), gaussian(.04))
 
 fun main() {
     val remote = remoteHub("定位优化").apply {
         openAllNetworks()
         println(networksInfo())
     }
-    val space by variableSpace("x", "y", "z")
-    val struct = BEACONS.associateWith { (space.ordinaryField - it.toPoint()).length() }
+    val locator = Locator(beacons)
 
-    val vx = Variable("x")
-    val vy = Variable("y")
-    val vz = Variable("z")
+    val upperRange = (1.5 * interval).roundToInt()
+    val zList = listOf(-2.6, -2.3, -2.0)
+    for (x in 0..upperRange)
+        for (y in if (x % 2 == 0) x..upperRange else upperRange downTo x)
+            for (z in if ((y - x) % 2 == 0) zList else zList.asReversed()) {
+                val mobile = vector3D(x, y, z)
+                val errors = (1..100).map {
+                    val map = beacons.map(::deploy)
+                    val result = map
+                        .map { p -> measure(p euclid mobile).takeIf { it < maxMeasure } ?: -1.0 }
+                        .let(locator::invoke)
 
-    val upperRange = (2 * interval).roundToInt()
-    for (x in 0..upperRange) for (y in x..upperRange) for (z in -3..-1) {
-        // 移动标签
-        val mobile = vector3D(x, y, z)
-        val map0 = BEACONS.associateWith(mobile::euclid).filterValues { it < maxMeasure }
-        val errors = (1..100).map {
-            // 测量
-            val map = map0.mapValues { (_, d) -> measure(d) }
-            // 求损失函数
-            val error = map.entries.sumBy { (b, e) -> e - struct.getValue(b) `^` 2 } / map.size
+                    remote.paintFrame3("目标", map.map { listOf(mobile, it) })
+                    remote.paintFrame3("定位", beacons.map { listOf(result, it) })
 
-            val f = dampingNewton(error, space)
-            remote.paint("目标点", mobile.x, mobile.y, mobile.z)
-            remote.paint("鞍点", mobile.x, mobile.y, 0)
-            //求解
-            val result =
-                recurrence(vector3D(0, 0, -5).toPoint() to .0) { (p, _) -> f(p) }
-                    .firstOrLast { (_, s) -> abs(s) < 5e-4 }
-                    .first
-                    .let { result ->
-                        val zz = result[vz]!!.toDouble()
-                        result.takeIf { zz > 0 }
-                            ?.expressions
-                            ?.toMutableMap()
-                            ?.also { it[vz] = Constant(-zz) }
-                            ?.let(::ExpressionVector)
-                        ?: result
-                    }
-            remote.paint(result - mobile.toPoint())
-            result.let { vector3D(it[vx]!!.toDouble(), it[vy]!!.toDouble(), it[vz]!!.toDouble()) } - mobile
-        }
-        val (a, e, d) = errors.statistic()
-        println("${map0.size} | ${mobile.rowView()} -> ${a.rowView()} | ${e.rowView()} | ${d.rowView()}")
-    }
+                    result - mobile
+                }
+                val (a, e, d) = errors.statistic()
+                remote.paint("路径", x, y, z)
+                remote.paint("均值", x + e.x, y + e.y, z + e.z)
+                println("${mobile.rowView()} -> ${a.rowView()} | ${e.rowView()} | ${d.rowView()}")
+            }
 }
 
-private fun Collection<Vector>.statistic(): Triple<Vector, Vector, Vector> {
+private fun Collection<Vector3D>.statistic(): Triple<Vector, Vector, Vector> {
     var abs = first().toList().map(::abs).toListVector()
     var expect = first()
     var square = first().toList().map { it * it }.toListVector()
@@ -103,8 +76,8 @@ private fun Collection<Vector>.statistic(): Triple<Vector, Vector, Vector> {
     }
     abs /= size
     expect /= size
-    square = square.toList().zip(expect.toList()) { a, b ->
+    val sigma = square.toList().zip(expect.toList()) { a, b ->
         (a / size - b * b).takeUnless { doubleEquals(it, .0) }?.let(::sqrt) ?: .0
     }.toListVector()
-    return Triple(abs, expect, square)
+    return Triple(abs, expect, sigma)
 }
