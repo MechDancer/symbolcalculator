9c3c3d5dc3f08e95013aa764d3ea7331e808526f
Ydr <ydrml@hotmail.com>
2020-03-09 22:40:15 +0800

    实现全联通子图筛选

--- src/test/kotlin/org/mechdancer/symbol/system/LocatingSystem.kt
+++ src/test/kotlin/org/mechdancer/symbol/system/LocatingSystem.kt
@@ -1,6 +1,7 @@
 package org.mechdancer.symbol.system
 
 import org.mechdancer.algebra.implement.vector.Vector3D
+import org.mechdancer.algebra.implement.vector.vector3DOfZero
 import java.util.*
 import kotlin.random.Random
 
@@ -9,7 +10,7 @@ class LocatingSystem {
         hashMapOf<Beacon, SortedMap<Long, Vector3D>>()
 
     private val relations =
-        hashMapOf<Position, HashSet<Position>>()
+        hashMapOf<Position, SortedSet<Position>>()
 
     private val measures =
         hashMapOf<Pair<Position, Position>, SortedMap<Long, Double>>()
@@ -25,28 +26,51 @@ class LocatingSystem {
         }
         positions.update(a.beacon, { it += a.time }, { sortedMapOf(a.time to random3D) })
         positions.update(b.beacon, { it += b.time }, { sortedMapOf(b.time to random3D) })
-        relations.update(u, { it += v }, { hashSetOf(v) })
-        relations.update(v, { it += u }, { hashSetOf(u) })
+        relations.update(u, { it += v }, { sortedSetOf(v) })
+        relations.update(v, { it += u }, { sortedSetOf(u) })
         measures.update(u to v, { it[t] = d }, { sortedMapOf(t to d) })
     }
 
     /** 使用所有已知的测量数据，优化所有坐标 */
     fun optimize() {
-        TODO()
+        calculate(measures.mapValues { (_, list) -> list.average() })
     }
 
     /** 获得一个全联通子图的全部定位 */
     operator fun get(beacon: Beacon): Map<Beacon, Vector3D> {
-        TODO()
+        val lastTime = positions[beacon]?.lastKey() ?: return mapOf(beacon to vector3DOfZero())
+        val position = beacon.move(lastTime)
+        val candidates = relations[position]!!
+        return candidates
+            .filterIndexed { i, p ->
+                relations[p]!!.containsAll(candidates.drop(i + 1))
+            }
+            .toSortedSet()
+            .apply { add(position) }
+            .toList()
+            .run {
+                sequence {
+                    for (i in indices) for (j in i + 1 until size)
+                        yield(get(i) to get(j))
+                }
+            }
+            .associateWith { measures[it]!!.average() }
+            .let(::calculate)
+            .mapKeys { (key, _) -> key.beacon }
     }
 
     /** 获得一份深拷贝的坐标系 */
     fun copy() = positions.mapValues { (_, map) -> map.toSortedMap() }
 
-    companion object {
-        private operator fun <T, U> SortedMap<T, U>.plusAssign(key: T) = set(key, get(lastKey()))
-        private val random3D get() = Vector3D(Random.nextDouble(), Random.nextDouble(), Random.nextDouble())
-        private fun <TK, TV> HashMap<TK, TV>.update(
+    /** 使用关心的部分关系更新坐标 */
+    private fun calculate(infomation: Map<Pair<Position, Position>, Double>)
+        : Map<Position, Vector3D> = TODO()
+
+    private companion object {
+        operator fun <T, U> SortedMap<T, U>.plusAssign(key: T) = set(key, get(lastKey()))
+        val random3D get() = Vector3D(Random.nextDouble(), Random.nextDouble(), Random.nextDouble())
+        fun SortedMap<Long, Double>.average() = values.sum() / size
+        fun <TK, TV> HashMap<TK, TV>.update(
             key: TK,
             block: (TV) -> Unit,
             default: () -> TV
--- src/test/kotlin/org/mechdancer/symbol/system/Position.kt
+++ src/test/kotlin/org/mechdancer/symbol/system/Position.kt
@@ -1,5 +1,8 @@
 package org.mechdancer.symbol.system
 
+import org.mechdancer.symbol.core.Variable
+import org.mechdancer.symbol.linear.ExpressionVector
+
 /**
  * 每个定位点对应标签的一次有意义的移动
  */
@@ -7,8 +10,22 @@ data class Position(
     val beacon: Beacon,
     val time: Long
 ) : Comparable<Position> {
+    fun toVector(): ExpressionVector {
+        val postfix = if (time > 0) "${beacon.id}_$time" else beacon.id.toString()
+        return ExpressionVector(mapOf(
+            x to Variable("x$postfix"),
+            y to Variable("y$postfix"),
+            z to Variable("z$postfix")))
+    }
+
     override fun compareTo(other: Position) =
         beacon.compareTo(other.beacon)
             .takeIf { it != 0 }
         ?: time.compareTo(other.time)
+
+    private companion object {
+        val x = Variable("x")
+        val y = Variable("y")
+        val z = Variable("z")
+    }
 }
